cabacd70c899fc2b67bfbc97b4a1f3efd1eb3559
yash mantri <mantriyash2025@gmail.com>
2020-04-09 12:46:48 +0530

    first commit

--- src/components/Info.js
+++ src/components/Info.js
@@ -22,7 +22,7 @@ function Pro() {
 
             <p></p>
 
-            <div style={{alignSelf:"center",paddingLeft:"23px"}}>
+            <div style={{alignSelf:"center",paddingLeft:"25px"}}>
             <img
                 src={require('./f.png')}
                 alt="avatar"
